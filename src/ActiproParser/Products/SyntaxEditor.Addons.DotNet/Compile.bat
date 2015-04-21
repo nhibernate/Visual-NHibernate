67f943518c2c9a26918bb7046b23a0f4c2278289
Ricardo Peres <rjperes@hotmail.com>
2015-04-21 10:32:35 +0100

    Removed unnecessary files

--- src/ActiproParser/Products/SyntaxEditor.Addons.DotNet/Compile.bat
+++
@@ -1,7 +0,0 @@
-cd "C:\Code\ActiproSoftware\SyntaxEditor4\Addons.DotNet\Products\SyntaxEditor.Addons.DotNet"
-
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-
-resgen Strings.txt
-
-pause
